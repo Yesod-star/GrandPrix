36d4b18be4cdb5f6f7ada0372912cacff1326341
Yesod-star <gabrielalmeidaci@hotmail.com>
2025-04-06 01:58:13 -0300

    versao aluno

--- js/script.js
+++ js/script.js
@@ -6,11 +6,7 @@ document.getElementById("contact-form").addEventListener("submit", async functio
   const email = form.email.value.trim();
   const message = form.message.value.trim();
 
-  // Validação simples
-  if (!name || !email || !message) {
-    alert("Por favor, preencha todos os campos.");
-    return;
-  }
+  // Validação simples se todos os campos estão preenchidos
 
   // Validação de e-mail básica
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -28,6 +24,7 @@ document.getElementById("contact-form").addEventListener("submit", async functio
       body: formData
     });
 
+    //Trocar para algo mais amigavel ao usuario, como trocar o texto do botão para enviado
     if (response.ok) {
       alert("Mensagem enviada com sucesso!");
       form.reset();
